b7c5eff57fcc53b0493a2b2aeee29b3660e95f43
Damian <dpendrak@gmail.com>
2022-01-30 15:05:28 +0100

    Try end result

--- src/collections/operations/find.ts
+++ src/collections/operations/find.ts
@@ -139,7 +139,24 @@ async function find<T extends TypeWithID = any>(incomingArgs: Arguments): Promis
 
   if (sortProperty.includes('.')) {
     collectionsAggregate
-      .unwind('authors');
+      .unwind('authors')
+      .addFields({
+        albumId: { $toObjectId: '$album.value' },
+        authorId: { $toObjectId: '$authors.value' },
+      })
+      .lookup({
+        from: 'albums',
+        localField: 'albumId',
+        foreignField: '_id',
+        as: 'album_docs',
+      })
+      .lookup({
+        from: 'authors',
+        localField: 'authorId',
+        foreignField: '_id',
+        as: 'author_docs',
+      })
+      .unwind('album_docs');
   }
 
   const paginatedDocs = await Model.aggregatePaginate(collectionsAggregate, optionsToExecute);
